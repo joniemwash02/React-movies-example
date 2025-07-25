47d5d90183b6eae2f5ae29a49bc59fc5c3809dfd
joniemwash02 <joniemwash2@gmail.com>
2025-07-26 01:49:06 +0300

    first commit

---
+++ movie/src/components/Moviecard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Moviecard = ({movie}) => {
+    const onfavoriteClick= ()=>{
+        alert("clicked")
+    }
+  return (
+    <div className='movie-card'>
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title} />
+            <div className="movie-overlay">
+                <button className='favorite-btn' onClick={onfavoriteClick}>
+                   🤍
+                </button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Moviecard

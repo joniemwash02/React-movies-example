05343fc42cfa4b5dc0025ef3733e96060479ffc7
joniemwash02 <joniemwash2@gmail.com>
2025-07-26 18:08:28 +0300

    creating Featch API

---
+++ src/services/Api.js
@@ -0,0 +1,16 @@
+const API_KEY = "d9b0389a4a92d3af711d16d102147b00"
+const BASE_URL = 'https://api.themoviedb.org/3'
+// https://api.themoviedb.org/3/search/movie?query=Batman&callback=test
+
+export const getPopularMovies = async () => {
+    const responce = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await responce.json();
+    return data.results
+}
+
+export const searchMovie = async (query) => {
+    const responce = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await responce.json();
+    return data.results
+}
+
